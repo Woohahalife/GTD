5edacdf17dfd1aee09776daa79cfb2663ae8b638
Geon.C <chlrjs1324@gmail.com>
2024-01-11 14:19:28 +0900

    Initial commit

---
+++ src/test/java/com/core/gtd/GtdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.core.gtd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GtdApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
