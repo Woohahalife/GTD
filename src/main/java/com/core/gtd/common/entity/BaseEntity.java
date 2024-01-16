4076b43c6ff1af3840e9bb33534fa64abd42ace0
Geon.C <chlrjs1324@gmail.com>
2024-01-16 23:49:34 +0900

    [feat] : GlobalExceptionHandler Unchecked Exception 처리 코드 추가
    Exception.class -> InternalServerError처리

--- src/main/java/com/core/gtd/common/entity/BaseEntity.java
+++ src/main/java/com/core/gtd/common/entity/BaseEntity.java
@@ -1,20 +1,42 @@
 package com.core.gtd.common.entity;
 
 
-import jakarta.persistence.MappedSuperclass;
+import jakarta.persistence.*;
 import lombok.Getter;
+import lombok.ToString;
+import org.hibernate.sql.Update;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
+import java.time.LocalDate;
 import java.time.LocalDateTime;
+import java.time.LocalTime;
 
 @Getter
+@ToString
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
 public class BaseEntity {
 
     @CreatedDate
-    private LocalDateTime createAt;
+    @Column(updatable = false, columnDefinition = "TIMESTAMP")
+    private LocalDateTime createdAt;
 
     @LastModifiedDate
-    private LocalDateTime updateAt;
+    @Column(columnDefinition = "TIMESTAMP")
+    private LocalDateTime updatedAt;
+
+//    @PrePersist
+//    public void prePersist() {
+//        createdAt = LocalDateTime.of(LocalDate.now(), LocalTime.MIDNIGHT);
+//
+//        updatedAt = createdAt;
+//    }
+//
+//    @PreUpdate
+//    public void preUpdate() {
+//        // 시:분:초를 00:00:00으로 초기화
+//        updatedAt = LocalDateTime.of(LocalDate.now(), LocalTime.MIDNIGHT);
+//    }
 }
--- src/main/java/com/core/gtd/common/exception/GlobalExceptionHandler.java
+++ src/main/java/com/core/gtd/common/exception/GlobalExceptionHandler.java
@@ -1,26 +1,33 @@
 package com.core.gtd.common.exception;
 
 import com.core.gtd.common.response.BaseResponse;
+import com.core.gtd.common.response.ResponseStatus;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 @Slf4j
 @RestControllerAdvice
-@RequiredArgsConstructor
 public class GlobalExceptionHandler {
 
     @ExceptionHandler(AppException.class)
     public ResponseEntity<BaseResponse> appExceptionHandler(AppException e) {
         log.warn("[AppException Occurs] message: {} HttpStatusCode: {}", e.getStatus().getMessage(),
                 e.getStatus().getStatusCode());
+
         return ResponseEntity.status(e.getStatus().getStatusCode())
                 .body(BaseResponse.response(e.getStatus()));
     }
 
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<BaseResponse> exceptionHandler(Exception e) {
+        log.error("[InternalServerError Occurs] error: {}", e.getMessage());
 
-
+        return ResponseEntity.internalServerError()
+                .body(BaseResponse.response(ResponseStatus.INVALID_ERROR));
+    }
 
 }
