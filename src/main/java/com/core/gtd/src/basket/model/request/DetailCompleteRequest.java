d09a7a92906861b30c2ae65c7ded09b36b865f89
Geon.C <chlrjs1324@gmail.com>
2024-01-24 19:23:31 +0900

    [feat] : TaskDetail 완료처리 및 미완료 변경 기능 구현
    조건문을 통해 보유한 IsComplete 데이터 검증 결과에 따라 데이터를 변경하는 로직 구성
    
    관련 요청 값을 구체화 하기 위해 DetailCompleteRequest 생성

---
+++ src/main/java/com/core/gtd/src/basket/model/request/DetailCompleteRequest.java
@@ -0,0 +1,16 @@
+package com.core.gtd.src.basket.model.request;
+
+import com.core.gtd.common.constatnt.IsComplete;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.ToString;
+
+@Getter
+@ToString
+@NoArgsConstructor
+@AllArgsConstructor
+public class DetailCompleteRequest {
+
+    private IsComplete isComplete;
+}
