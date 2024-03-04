eede4bb96e593872656d931d5477f768fea6e1cd
Geon.C <chlrjs1324@gmail.com>
2024-03-04 19:41:22 +0900

    [feat] : Principal 객체 구현
    principal 클래스는 인증된 사용자의 정보를 담는 클래스로서 시큐리티의 인증 및 권한 부여에 사용할 목적으로 생성

---
+++ src/main/java/com/core/gtd/src/common/security/principal/UserPrincipal.java
@@ -0,0 +1,61 @@
+package com.core.gtd.src.common.security.principal;
+
+import com.core.gtd.src.users.entity.Users;
+import lombok.RequiredArgsConstructor;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.Optional;
+
+@RequiredArgsConstructor
+public class UserPrincipal implements UserDetails {
+
+    private final Users users;
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+
+        Collection<GrantedAuthority> collection = new ArrayList<>();
+
+        collection.add((GrantedAuthority) () -> users.getRole().name());
+
+        return collection;
+    }
+
+    public Optional<Users> getUsers() {
+
+        return Optional.ofNullable(users);
+    }
+
+    @Override
+    public String getPassword() {
+        return users.getPassword();
+    }
+
+    @Override
+    public String getUsername() {
+        return users.getUsername();
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return true;
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return true;
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return true;
+    }
+}
